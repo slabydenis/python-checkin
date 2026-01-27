826a7484dba9982bba7cad1c8099d7090894906e
slabydenis <Denis.Varga@t-systems.com>
2026-01-27 10:30:59 +0300

    Pridanie cesty k scriptom

--- repo-automation/git-actions/test-git-actions.sh
+++ repo-automation/git-actions/test-git-actions.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+echo "Cesta k scriptom"
+oldpwd="$(pwd)"
+function on_exit () {
+  cd "$oldpwd"
+}
+trap on_exit EXIT ERR SIGINT SIGTERM
+cd "$(dirname "$(realpath "$0")")"
+
+
 echo "Spúšťam všetky scripty"
 
 ./make-changes.sh
