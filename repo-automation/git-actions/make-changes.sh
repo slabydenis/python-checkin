77e5cf78b2a7d836fc41191585116981f520bd44
slabydenis <Denis.Varga@t-systems.com>
2026-01-27 10:17:18 +0300

    Fix na RANDOM hash

--- repo-automation/git-actions/make-changes.sh
+++ repo-automation/git-actions/make-changes.sh
@@ -3,6 +3,7 @@
 FILE="zmeny.txt"
 
 echo "Timestamp: $(date)" > "$FILE"
-echo "Random hash: $(echo $RANDOM)" >> "$FILE"
+echo "Random hash: $(head -c 32 /dev/urandom | sha256sum | cut -c1-16)" >> "$FILE"
+
 
 echo "Súbor $FILE bol vytvoreny/aktualizovany"
